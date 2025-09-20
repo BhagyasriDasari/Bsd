fb51c183dc1ce19510d251dd4008b304c365049a
BhagyasriDasari <106796145+BhagyasriDasari@users.noreply.github.com>
2025-09-20 16:00:05 +0530

    updated mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,18 @@
-// backend/server.js
-const app = require("./app");
+// server.js (top of file)
+require("dotenv").config(); // <-- load .env first
+const express = require("express");
+const connectDB = require("./config/db");
 
+// optional debug
+console.log("DEBUG: MONGO_URI =", process.env.MONGO_URI);
+
+const app = express();
 const PORT = process.env.PORT || 5000;
 
+// connect DB
+connectDB();
+
+// your routes...
 app.listen(PORT, () => {
   console.log(`Backend running at http://localhost:${PORT}`);
 });
